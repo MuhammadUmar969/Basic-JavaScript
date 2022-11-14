efb143b9c1c1fe87f927ab102e3a7539a6a9e4dc
Muhammad Umar Aziz Khan <105535313+MuhammadUmar969@users.noreply.github.com>
2022-11-14 22:19:30 +0500

    Spread Operator and Destruction

---
+++ script.js
@@ -0,0 +1,24 @@
+arr = [1,2,4]
+
+function avg(a,b,c){
+    return a+b+c/3
+}
+
+//let a=  avg (...arr)
+
+
+// Spread Operator
+ //let a = [4 , 7,...arr,67];
+ //let a = [...arr];
+ //let a2 =[5,7,9];
+ //let a =[...arr,...a2];
+// console.log(a)
+ let obj1 ={
+    name: "Umar",
+    class: "1c",
+    lang: "JavaScript"
+ }
+ let obj2 ={...obj1 , lang: "Java"};
+ console.log(obj2)
+ let {name1,class1 ,lang} =obj1;//Destruction
+ console.log(obj1)
